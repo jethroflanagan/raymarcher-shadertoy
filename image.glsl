854e3116ceff617ea8b8244d644072bf7820cf7a
Jethro Flanagan <jethroflanagan@gmail.com>
2020-08-17 22:07:31 +0200

    Adds camera control and box rotation

--- image.glsl
+++ image.glsl
@@ -36,6 +36,22 @@ struct Box {
   float distance;
 };
 
+mat2 rotate(float a) {
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c, -s, s, c);
+}
+
+vec3 R(vec2 uv, vec3 p, vec3 l, float z) {
+    vec3 f = normalize(l-p),
+        r = normalize(cross(vec3(0,1,0), f)),
+        u = cross(f,r),
+        c = p+f*z,
+        i = c + uv.x*r + uv.y*u,
+        d = normalize(i-p);
+    return d;
+}
+
 Sphere signedDistanceSphere(vec3 point, vec3 position, float radius) {
   float distance = length(point - position) - radius;
   return Sphere(position, radius, distance);
@@ -91,7 +107,12 @@ Torus signedDistanceTorus(vec3 point, vec3 position, float radius, float innerRa
 }
 
 Box signedDistanceBox(vec3 point, vec3 position, vec3 size) {
-  float distance = length(max(abs(point - position) - size, 0.));
+  vec3 boxPosition = (point - position);
+  boxPosition.xz *= rotate(iTime);
+  // boxPosition.yz *= rotate(iTime*.4);
+  // boxPosition.yx *= rotate(iTime*.4);
+
+  float distance = length(max(abs(boxPosition) - size, 0.));
   return Box(
     position,
     size,
@@ -100,18 +121,20 @@ Box signedDistanceBox(vec3 point, vec3 position, vec3 size) {
 }
 
 float getDistanceToScene(vec3 point) {
-    Sphere sphere = signedDistanceSphere(point, vec3(0, 1, 6), 1.);
+    Sphere sphere = signedDistanceSphere(point, vec3(0, 1, 0), 1.);
 
-    Capsule capsule = signedDistanceCapsule(point, vec3(0, .7, 6), vec3(1, 2, 6), .2);
+    Capsule capsule = signedDistanceCapsule(point, vec3(0, .7, 0), vec3(1, 2, 0), .2);
 
-    Cylinder cylinder = signedDistanceCylinder(point, vec3(1.5, .1, 4), vec3(.1, 1.2, 4), .4);
+    Cylinder cylinder = signedDistanceCylinder(point, vec3(1.5, .1, 2), vec3(.1, 1.2, 2), .4);
 
-    Torus torus = signedDistanceTorus(point, vec3(0, 0.2, 5), 1.5, .1);
+    Torus torus = signedDistanceTorus(point, vec3(0, 0.2, 1), 1.5, .1);
 
-    Box box = signedDistanceBox(point, vec3(-1, .15, 6), vec3(.5, .3, 1.9));
+    Box box = signedDistanceBox(point, vec3(-1, .15, 0), vec3(.5, .3, 1.9));
+
 
     float distanceToPlane = point.y;
     float distance = min(capsule.distance, distanceToPlane);
+    distance = min(distance, sphere.distance);
     distance = min(distance, torus.distance);
     distance = min(distance, box.distance);
     distance = min(distance, cylinder.distance);
@@ -123,7 +146,6 @@ vec3 getNormal(vec3 point) {
   vec2 epsilon = vec2(.01, 0);
   float distance = getDistanceToScene(point);
 
-  // swizzles
   vec3 normal = distance - vec3(
     getDistanceToScene(point - epsilon.xyy),
     getDistanceToScene(point - epsilon.yxy),
@@ -150,7 +172,7 @@ float rayMarch(vec3 rayOrigin, vec3 rayDistance) {
 }
 
 float getLight(vec3 point) {
-  vec3 lightPosition = vec3(0, 5, 6);
+  vec3 lightPosition = vec3(0, 5, 0);
   lightPosition.xz += vec2(sin(iTime), cos(iTime) * 2.);
 
   vec3 light = normalize(lightPosition - point);
@@ -172,17 +194,22 @@ float getLight(vec3 point) {
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     // Normalized pixel coordinates (from 0 to 1)
     vec2 uv = (fragCoord - .5 * iResolution.xy) / iResolution.y;
+    vec2 mousePosition = iMouse.xy / iResolution.xy;
 
     // camera
-    vec3 rayOrigin = vec3(0, 2, 0);
-    vec3 rayDistance = normalize(vec3(uv.x, uv.y - .2, 1));
+    vec3 rayOrigin = vec3(0, 4, -5);
+    rayOrigin.yz *= rotate(-mousePosition.y + .4);
+    rayOrigin.xz *= rotate(iTime*.2 - mousePosition.x*6.2831);
+
+    // vec3 rayDistance = normalize(vec3(uv.x, uv.y - .2, 1));
+    vec3 rayDistance = R(uv, rayOrigin, vec3(0), .7);
 
     float distance = rayMarch(rayOrigin, rayDistance);
 
     vec3 point = rayOrigin + rayDistance * distance;
     float diffuseLight = getLight(point);
 
-    vec3 col = vec3(diffuseLight);
+    vec3 col = vec3(diffuseLight, 0., 0.4);
     // vec3 col = getNormal(point);
 
     fragColor = vec4(col, 1.0);
