ec83766841039fe5817964080b286d73df74b922
Jethro Flanagan <jethroflanagan@gmail.com>
2020-08-20 00:36:13 +0200

    Adds boolean ops and smooth union

--- image.glsl
+++ image.glsl
@@ -1,3 +1,4 @@
+// ref: Art of Code https://www.youtube.com/watch?v=AfKGMUDWfuE
 #define SURFACE_DISTANCE_EPSILON 0.01
 #define MAX_STEPS 100
 #define MAX_DISTANCE 100.
@@ -52,6 +53,40 @@ vec3 R(vec2 uv, vec3 p, vec3 l, float z) {
     return d;
 }
 
+// polynomial, taken from Dreams (ref: https://www.iquilezles.org/www/articles/smin/smin.htm)
+float smoothMinPoly(float a, float b, float smoothness) {
+  float h = max( smoothness-abs(a-b), 0.0 )/smoothness;
+  return min( a, b ) - h*h*smoothness*(1.0/4.0);
+}
+// exponential, ref: https://www.iquilezles.org/www/articles/smin/smin.htm
+float smoothMinExpo(float a, float b, float smoothness) {
+  float res = exp2( -smoothness*a ) + exp2( -smoothness*b );
+    return -log2( res )/smoothness;
+}
+
+// power, ref: https://www.iquilezles.org/www/articles/smin/smin.htm
+float smoothMinPow(float a, float b, float smoothness) {
+  a = pow(a, smoothness); b = pow(b, smoothness);
+  return pow((a * b) / (a + b), 1.0 / smoothness);
+}
+
+float booleanSubtract(float distanceA, float distanceB) {
+  return max(-distanceB, distanceA);
+}
+
+float booleanIntersection(float distanceA, float distanceB) {
+  return max(distanceB, distanceA);
+}
+
+float booleanUnion(float distanceA, float distanceB) {
+  return min(distanceB, distanceA);
+}
+
+float booleanUnionSmooth(float distanceA, float distanceB, float smoothness) {
+  return smoothMinPoly(distanceB, distanceA, smoothness);
+}
+
+
 Sphere signedDistanceSphere(vec3 point, vec3 position, float radius) {
   float distance = length(point - position) - radius;
   return Sphere(position, radius, distance);
@@ -94,10 +129,14 @@ Cylinder signedDistanceCylinder(vec3 point, vec3 a, vec3 b, float radius) {
   );
 }
 
-Torus signedDistanceTorus(vec3 point, vec3 position, float radius, float innerRadius) {
+Torus signedDistanceTorus(vec3 point, vec3 position, float radius, float innerRadius, float offset) {
   vec3 relativePosition = point - position;
+  relativePosition.xy *= rotate(sin(iTime) + offset);
+  relativePosition.zy *= rotate(iTime + offset);
+  // relativePosition.xy += vec2(sin(iTime), cos(iTime) * 2.);
   float x = length(relativePosition.xz) - radius;
   float distance = length(vec2(x, relativePosition.y)) - innerRadius;
+  
   return Torus(
     position,
     innerRadius,
@@ -109,10 +148,10 @@ Torus signedDistanceTorus(vec3 point, vec3 position, float radius, float innerRa
 Box signedDistanceBox(vec3 point, vec3 position, vec3 size) {
   vec3 boxPosition = (point - position);
   boxPosition.xz *= rotate(iTime);
-  // boxPosition.yz *= rotate(iTime*.4);
-  // boxPosition.yx *= rotate(iTime*.4);
-
-  float distance = length(max(abs(boxPosition) - size, 0.));
+  boxPosition.yz *= rotate(iTime*.4);
+  boxPosition.yx *= rotate(iTime*.4);
+  float stepDivision = 4.; // to cater for rotation causing artifacts
+  float distance = length(max(abs(boxPosition) - size, 0.)) / stepDivision;
   return Box(
     position,
     size,
@@ -122,22 +161,34 @@ Box signedDistanceBox(vec3 point, vec3 position, vec3 size) {
 
 float getDistanceToScene(vec3 point) {
     Sphere sphere = signedDistanceSphere(point, vec3(0, 1, 0), 1.);
+    Sphere sphere2 = signedDistanceSphere(point, vec3(1., 1, 0), 1.);
 
     Capsule capsule = signedDistanceCapsule(point, vec3(0, .7, 0), vec3(1, 2, 0), .2);
 
     Cylinder cylinder = signedDistanceCylinder(point, vec3(1.5, .1, 2), vec3(.1, 1.2, 2), .4);
 
-    Torus torus = signedDistanceTorus(point, vec3(0, 0.2, 1), 1.5, .1);
+    Torus torus = signedDistanceTorus(point, vec3(0, 0.2, 1), 1.5, .2, 0.);
+    Torus torus2 = signedDistanceTorus(point, vec3(0, 0.2, 1), .7, .1, 1.);
 
     Box box = signedDistanceBox(point, vec3(-1, .15, 0), vec3(.5, .3, 1.9));
 
+    // boolean modifications
+    float sDistance = booleanUnionSmooth(box.distance, sphere.distance, .5);
+    sDistance = booleanUnionSmooth(sDistance, capsule.distance, 0.3);
+    sDistance = booleanUnionSmooth(sDistance, torus.distance, 0.3);
+    sDistance = booleanUnionSmooth(sDistance, torus2.distance, 0.3);
+    sDistance = booleanUnionSmooth(sDistance, cylinder.distance, 0.3);
 
+    // scene distance
     float distanceToPlane = point.y;
-    float distance = min(capsule.distance, distanceToPlane);
-    distance = min(distance, sphere.distance);
-    distance = min(distance, torus.distance);
-    distance = min(distance, box.distance);
-    distance = min(distance, cylinder.distance);
+    float distance = booleanUnionSmooth(sDistance, distanceToPlane, .3);
+    // float distance = min(capsule.distance, distanceToPlane);
+    // distance = min(distance, sphere2.distance);
+    // distance = min(distance, sphere.distance);
+    // distance = min(distance, box.distance);
+    // distance = min(distance, sDistance);
+    // distance = min(distance, torus.distance);
+    // distance = min(distance, cylinder.distance);
 
     return distance;
 }
@@ -199,7 +250,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     // camera
     vec3 rayOrigin = vec3(0, 4, -5);
     rayOrigin.yz *= rotate(-mousePosition.y + .4);
-    rayOrigin.xz *= rotate(iTime*.2 - mousePosition.x*6.2831);
+    rayOrigin.xz *= rotate(- mousePosition.x*6.2831);
 
     // vec3 rayDistance = normalize(vec3(uv.x, uv.y - .2, 1));
     vec3 rayDistance = R(uv, rayOrigin, vec3(0), .7);
@@ -214,4 +265,3 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 
     fragColor = vec4(col, 1.0);
 }
-
